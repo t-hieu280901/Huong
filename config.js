28cb31c4876577a98c528bb68e96becd308094ce
t-hieu280901 <88757783+t-hieu280901@users.noreply.github.com>
2021-08-11 11:24:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,18 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bé ơi!',
+    introDesc: `Pardon the way that I stare
+There’s nothing else to compare
+The sight of you leaves me weak
+There are no words left to speak
+But if you feel like I feel
+Please let me know that it’s real
+You’re just to good to be true
+And my baby
+Can’t take my eyes off of you`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'i Love you bae <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
