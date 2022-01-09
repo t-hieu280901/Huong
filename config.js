268d086005df687d603cce4c38cbbb9501ac63f6
t-hieu280901 <88757783+t-hieu280901@users.noreply.github.com>
2022-01-09 12:54:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hello Pé Hương!',
+    introDesc: `Anh gọi chỉ để hỏi cơm hôm nay ăn chưa
+    Nếu rồi thì đã húp canh chưa
+    Húp rồi thì đã nhớ anh chưa :)))))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Đâu phải ai là a cũng gởi cái này đ',
+    desc: 'Đi chơi hem nè :)))) ',
+    btnYes: 'Okieeeeeeee a nuôn nhó <3',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question:'Nói gì đi chòi :))))) ',
+    btnReply: 'Gửi cho Híuthiunang <3',
+    reply: 'Ai bít',
+    mess: 'Ừa hihihihiihihihihihi',
+    messDesc: 'Paiiiiiiiii.',
+    btnAccept: 'Paiiiiiiiiiii <3',
+    messLink: https://www.facebook.com/Hiutran123
 }
